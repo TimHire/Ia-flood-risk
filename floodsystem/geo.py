b48de41c9412795b6ff9a3afe74fe88699538d5b
Tim H <tph41@cam.ac.uk>
2022-01-22 11:35:03 +0000

    task 1B: sording stations

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,12 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from haversine import haversine
+
+def stations_by_distance(stations, p):
+    output_list = []
+    for station in stations:
+        distance = haversine(station.coord, p)
+        output_list.append((station, distance))
+    sorted_by_key(output_list, 1)
+    return output_list
