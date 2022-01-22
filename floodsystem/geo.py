742131bdead7e525447390c60811c1054fdf7f71
Tim H <tph41@cam.ac.uk>
2022-01-22 12:28:22 +0000

    Task 1B comments on function

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -10,9 +10,17 @@ from .utils import sorted_by_key  # noqa
 from haversine import haversine
 
 def stations_by_distance(stations, p):
+    # Create empty list to append tuples of (station, distance) to
     output_list = []
+
+    # Iterate through all stations in the list to work out the distance between p and the station
     for station in stations:
+        # Get the coordinates of the station
         distance = haversine(station.coord, p)
+
+        # Append the tuple to the output list
         output_list.append((station, distance))
+
+    # Sort the list so by shortest distance
     sorted_by_key(output_list, 1)
     return output_list
