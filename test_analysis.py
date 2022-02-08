f735d01453a95e378dfe043f94de3533fd0b884b
Tim H <tph41@cam.ac.uk>
2022-02-08 14:52:13 +0000

    Should have fixed pytest tests

--- test_analysis.py
+++ test_analysis.py
@@ -4,15 +4,15 @@ import numpy as np
 
 def test_polyfit():
     dates = list(range(0, 50))
-    levels = [x^5 + 4 * x^4 - 2 * x + 156 for x in dates]
+    levels = [x**5 + 4 * x**4 - 2 * x + 156 for x in dates]
 
     n = 5
 
     p_coeff = np.polyfit(dates, levels, n)
     
-    assert p_coeff[0] == 1
-    assert p_coeff[1] == 4
-    assert p_coeff[2] == 0
-    assert p_coeff[3] == 0
-    assert p_coeff[4] == -2
-    assert p_coeff[5] == 156
+    assert round(p_coeff[0]) == 1
+    assert round(p_coeff[1]) == 4
+    assert round(p_coeff[2]) == 0
+    assert round(p_coeff[3]) == 0
+    assert round(p_coeff[4]) == -2
+    assert round(p_coeff[5]) == 156
--- test_plot.py
+++ test_plot.py
@@ -5,7 +5,7 @@ import numpy as np
 def testing_best_fit_plotting():
     """ Function for testing the plotting of the line of best fit """
     dates = list(range(0, 50))
-    levels = [x^5 + 4 * x^4 - 2 * x + 156 for x in dates]
+    levels = [x**5 + 4 * x**4 - 2 * x + 156 for x in dates]
 
     n = 5
 
