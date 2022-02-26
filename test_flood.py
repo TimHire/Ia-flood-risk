ad245c314d168f0c7c397b5d5ca2f739fe37ca02
Tim H <tph41@cam.ac.uk>
2022-02-26 14:43:00 +0000

    Updated testing files

--- test_flood.py
+++ test_flood.py
@@ -1,3 +1,16 @@
 # File for testing the flood submodule in floodsystem
 
-# File needs to be completed after Ian finishes his tasks
+def test_stations_level_over_threshold():
+    relative_water_level1 = 0.8
+    relative_water_level2 = 0.6
+    tolerance = 0.7
+
+    # Check whether logic correct for comparing relative water levels of each station against the defined tolerance value
+    assert relative_water_level1 > tolerance
+    assert relative_water_level2 < tolerance
+
+def test_stations_highest_rel_level():
+    station_number = 3
+    example_list = [1, 43, 4, 6, 8, 5]
+
+    assert example_list[:station_number] == [1, 43, 4]
